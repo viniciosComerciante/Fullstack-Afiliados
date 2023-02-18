f3275b84befb215fe5387bbab1d3f5827a53ceee
Vinícios Oliveira <vinicompif@gmail.com>
2023-02-18 14:49:12 -0300

    add validation on params and link prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot(), UsersModule],
+    controllers: [AppController],
+    providers: [AppService, PrismaService],
 })
 export class AppModule {}
