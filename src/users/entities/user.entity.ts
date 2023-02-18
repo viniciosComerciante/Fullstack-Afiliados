c962b2cdc0a9c30f79373358b2b1db1856308a0d
Vinícios Oliveira <vinicompif@gmail.com>
2023-02-18 15:02:18 -0300

    change users entity to implements users from prisma

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,10 @@
-export class User {
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
     id: string;
     name: string;
     email: string;
     password: string;
+    created_at: Date;
+    updated_at: Date;
 }
