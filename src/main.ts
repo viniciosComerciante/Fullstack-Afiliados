eb2673710eae1fb4e47282a13f17bda4c1b32ef1
Vinícios Oliveira <vinicompif@gmail.com>
2023-02-18 17:55:43 -0300

    error treatment removed from service layer

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
             transform: true,
         }),
     );
+    // app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalInterceptors(new UnauthorizedInterceptor());
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
