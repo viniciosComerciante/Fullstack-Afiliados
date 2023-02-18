c1abf88331722a91aa74f84279903252063a92f9
Vinícios Oliveira <vinicompif@gmail.com>
2023-02-18 14:51:41 -0300

    remove prisma service import from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { PrismaService } from './prisma/prisma.service';
 
 @Module({
     imports: [ConfigModule.forRoot(), UsersModule],
     controllers: [AppController],
-    providers: [AppService, PrismaService],
+    providers: [AppService],
 })
 export class AppModule {}
