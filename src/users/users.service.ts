dfe1196304583c24a1b0aefa8e972a079ae704df
Vinícios Oliveira <vinicompif@gmail.com>
2023-02-18 15:01:45 -0300

    fix users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,52 +1,59 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { User } from './entities/user.entity';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [
-    {
-      id: 'da5sda4sd5',
-      email: 'vinicompif@gmail.com',
-      name: 'vinicios da costa oliveira',
-      password: '545422sds6ad',
-    },
-    {
-      id: 'dsa54d4545dsdsd',
-      email: 'robertabarros14@gmail.com',
-      name: 'Roberta marcial barros',
-      password: '65d5sadsds',
-    },
-  ];
+    private users: UserEntity[] = [
+        {
+            id: 'da5sda4sd5',
+            email: 'vinicompif@gmail.com',
+            name: 'vinicios da costa oliveira',
+            password: '545422sds6ad',
+            created_at: new Date(),
+            updated_at: new Date(),
+        },
+        {
+            id: 'dsa54d4545dsdsd',
+            email: 'robertabarros14@gmail.com',
+            name: 'Roberta marcial barros',
+            password: '65d5sadsds',
+            created_at: new Date(),
+            updated_at: new Date(),
+        },
+    ];
 
-  findAll() {
-    return this.users;
-  }
-
-  findOne(id: string) {
-    const user = this.users.find((user) => user.id === id);
-    if (!user) {
-      throw new HttpException(`User ID ${id} not found`, HttpStatus.NOT_FOUND);
+    findAll() {
+        return this.users;
     }
-    return user;
-  }
 
-  create(createUserDto: any) {
-    return this.users.push(createUserDto);
-  }
+    findOne(id: string) {
+        const user = this.users.find(user => user.id === id);
+        if (!user) {
+            throw new HttpException(
+                `User ID ${id} not found`,
+                HttpStatus.NOT_FOUND,
+            );
+        }
+        return user;
+    }
 
-  update(id: string, updateUserDto: any) {
-    const indexUser = this.users.findIndex((user) => user.id === id);
+    create(createUserDto: any) {
+        return this.users.push(createUserDto);
+    }
 
-    if (indexUser === -1) return;
+    update(id: string, updateUserDto: any) {
+        const indexUser = this.users.findIndex(user => user.id === id);
 
-    return (this.users[indexUser] = updateUserDto);
-  }
+        if (indexUser === -1) return;
 
-  remove(id: string) {
-    const indexUser = this.users.findIndex((user) => user.id === id);
+        return (this.users[indexUser] = updateUserDto);
+    }
 
-    if (indexUser === -1) return;
+    remove(id: string) {
+        const indexUser = this.users.findIndex(user => user.id === id);
 
-    return this.users.splice(indexUser, 1);
-  }
+        if (indexUser === -1) return;
+
+        return this.users.splice(indexUser, 1);
+    }
 }
