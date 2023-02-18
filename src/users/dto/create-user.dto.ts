500e697a6fad6a65b66ae28d688ea739f3082689
Vinícios Oliveira <vinicompif@gmail.com>
2023-02-18 15:18:43 -0300

    validation data in create users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,15 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
-    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
     readonly email: string;
 
     @IsString()
+    @IsNotEmpty()
     readonly name: string;
 
     @IsString()
+    @IsNotEmpty()
     readonly password: string;
 }
