eacac46ba9ea2532181fa16741ff03f525e503c5
Vinícios Oliveira <vinicompif@gmail.com>
2023-02-20 16:51:08 -0300

    sales service

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -6,10 +6,16 @@ import {
     Patch,
     Param,
     Delete,
+    UseInterceptors,
+    UploadedFile,
+    ParseFilePipeBuilder,
+    HttpStatus,
 } from '@nestjs/common';
+
 import { SalesService } from './sales.service';
 import { CreateSaleDto } from './dto/create-sale.dto';
 import { UpdateSaleDto } from './dto/update-sale.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('sales')
 export class SalesController {
@@ -20,6 +26,24 @@ export class SalesController {
         return this.salesService.create(createSaleDto);
     }
 
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadFile(
+        @UploadedFile(
+            new ParseFilePipeBuilder()
+                .addFileTypeValidator({
+                    fileType: 'text/plain',
+                })
+                .addMaxSizeValidator({ maxSize: 1000000 })
+                .build({
+                    errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+                }),
+        )
+        file: Express.Multer.File,
+    ) {
+        return this.salesService.createMany(file);
+    }
+
     @Get()
     findAll() {
         return this.salesService.findAll();
@@ -27,16 +51,16 @@ export class SalesController {
 
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return this.salesService.findOne(+id);
+        return this.salesService.findOne(id);
     }
 
     @Patch(':id')
     update(@Param('id') id: string, @Body() updateSaleDto: UpdateSaleDto) {
-        return this.salesService.update(+id, updateSaleDto);
+        return this.salesService.update(id, updateSaleDto);
     }
 
     @Delete(':id')
     remove(@Param('id') id: string) {
-        return this.salesService.remove(+id);
+        return this.salesService.remove(id);
     }
 }
