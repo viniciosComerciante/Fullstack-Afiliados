a1db05979b24903446019e29c0ebb4c69f8da8b1
Vinícios Oliveira <vinicompif@gmail.com>
2023-02-20 23:35:30 -0300

    login page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,26 @@
-// import { Inter } from "@next/font/google";
+import { Input } from "@/components/Form/Input";
+import { Button, Flex, Stack } from "@chakra-ui/react";
 
-// const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return <h1>Namu Dashboard</h1>;
+export default function SignIn() {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxW={360}
+        bg="gray.800"
+        padding={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="E-mail" type="email"></Input>
+          <Input name="password" label="Senha" type="password"></Input>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="blue">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
